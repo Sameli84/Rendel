f8b238857f07741e8607a1e6a224ffca52f1891e
Sampsa Jarvinen <Sampsa.Jarvinen@tuni.fi>
2023-03-13 11:06:29 +0200

    parts 3 & 4

---
+++ frontend/src/cities/components/CitiesList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import CityItem from './CityItem';
+
+import './CitiesList.css';
+
+const CitiesList = props => {
+  return <ul className="cities-list">
+    {props.items.map(city => 
+      <CityItem 
+        key={city.id}
+        capital={city.capital}
+        country={city.country}
+        image={city.image}
+        id={city.id}
+      />
+    )}
+    </ul>
+};
+
+export default CitiesList;
